e947a7ce390a7870c473b3ec104e7bd199e39dc4
Aric Zhuang <ariczhuang@gmail.com>
2018-10-31 17:40:37 -0400

    hooked up with firebase yeet

--- commentapp/models.py
+++ commentapp/models.py
@@ -5,11 +5,12 @@ from pyrebase import initialize_app
 
 
 # Firebase
-FIREBASE_API_KEY = environ.get("FIREBASE_API_KEY", "no-key")
-FIREBASE_AUTH_DOMAIN = ""
-FIREBASE_DATABASE_URL = ""
-FIREBASE_STORAGE_BUCKET = ""
-FIREBASE_SERVICE_ACCOUNT = ""
+FIREBASE_API_KEY = "AIzaSyDNOXCCYb7VulBQx3sNFZ-ORaYEsvRRGP4"
+#environ.get("FIREBASE_API_KEY", "no-key")
+FIREBASE_AUTH_DOMAIN = "brown-ep-startup-ideas-app.firebaseapp.com"
+FIREBASE_DATABASE_URL = "https://brown-ep-startup-ideas-app.firebaseio.com"
+FIREBASE_STORAGE_BUCKET = "brown-ep-startup-ideas-app.appspot.com"
+FIREBASE_SERVICE_ACCOUNT = "brown-ep-startup-ideas-app-firebase-adminsdk-qu2p2-6f5baf423c.json"
 
 __config = {
     "apiKey": FIREBASE_API_KEY,
